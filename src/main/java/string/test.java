47a8908ac12c63be32dccbe9cb7f5ee9cbe37825
rqxiao <1007689622@qq.com>
2019-07-02 16:20:06 +0800

    New changelist

--- src/main/java/string/test.java
+++ src/main/java/string/test.java
@@ -2,7 +2,17 @@ package string;
 
 public class test {
     public static void main(String[] args) {
-        String a;
-        System.out.println("a");
+        String a="aaaa|vvvv";
+        String a1="aaaa|vvvv|sdasda";
+
+        String a2="bbbb";
+
+
+        System.out.println(a.contains("|"));
+        System.out.println(a1.contains("|"));
+
+
+        System.out.println(a2.contains("|"));
+
     }
 }

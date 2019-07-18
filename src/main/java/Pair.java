e16d45325cc69fc9d91a50fc22d408c0f6168085
rqxiao <1007689622@qq.com>
2019-07-18 17:36:57 +0800

    New changelist

--- src/main/java/Pair.java
+++ src/main/java/Pair.java
@@ -4,14 +4,14 @@ import java.util.Map;
 public class Pair<T> {
 
     public static void main(String[] args) {
-        /*Map<K,V> a=new HashMap<K,V>();
+        Map<String,String> a=new HashMap<String,String>();
         a.put("1","1111");
 
         a.put("2","22222");
 
         a.put("3","33333");
 
-*/
+        System.out.println(a);
     }
 
     private T value;

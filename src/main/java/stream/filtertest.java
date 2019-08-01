793b0ce59bea6d9608c9d0743486990c01af4160
rqxiao <1007689622@qq.com>
2019-08-01 13:57:02 +0800

    New changelist

--- src/main/java/stream/filtertest.java
+++ src/main/java/stream/filtertest.java
@@ -2,12 +2,24 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class filtertest {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
 
-        strings.add("333");
+//        strings.add("333");
+
+        List<String> stringList = strings.stream().filter(string -> "abc".equals(string)).collect(Collectors.toList());
+
+
+
+
+
+
+
         long count = strings.stream().filter(string -> { return string.isEmpty();}).count();
         System.out.println(count);
 

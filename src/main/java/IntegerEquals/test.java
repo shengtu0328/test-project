142883122a0ced6a4b26870005e305f8367316bb
rqxiao <1007689622@qq.com>
2019-09-19 15:50:42 +0800

    New changelist

---
+++ src/main/java/IntegerEquals/test.java
@@ -0,0 +1,20 @@
+package IntegerEquals;
+
+/**
+ * @Author: rqxiao
+ * @Date: 2019-8-8 09:51
+ * @Description:
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        Integer i=2;
+
+
+        System.out.println(1 == i);
+
+        System.out.println(2 == i);
+
+    }
+}

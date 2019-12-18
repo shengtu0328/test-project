40b913f81aede833f73568ede7d2157ec004dea1
rqxiao <1007689622@qq.com>
2019-12-18 13:50:09 +0800

    New changelist

--- src/main/java/lambda/Java8Examples3.java
+++ src/main/java/lambda/Java8Examples3.java
@@ -16,19 +16,23 @@ public class Java8Examples3 {
     public static void main(String[] args) {
 
         //3 apple, 2 banana, others 1
-    List<Item> items = Arrays.asList(
-            new Item("apple", 10, new BigDecimal("9.99")),
-            new Item("apple", 10, new BigDecimal("9.99")),
-            new Item("apple", 20, new BigDecimal("9.99")),
+    List<Item> items = new ArrayList();
 
-            new Item("banana", 10, new BigDecimal("19.99")),
-            new Item("banana", 20, new BigDecimal("19.99")),
+        items.add(new Item("1", 10, new BigDecimal("9.99")));
+        items.add(new Item("1", 10, new BigDecimal("9.99")));
+        items.add(new Item("1", 10, new BigDecimal("9.99")));
+        items.add(new Item("2", 10, new BigDecimal("9.99")));
+        items.add(new Item("2", 10, new BigDecimal("9.99")));
+        items.add(new Item("2", 10, new BigDecimal("9.99")));
+        items.add(new Item("3", 10, new BigDecimal("9.99")));
+        items.add(new Item("3", 10, new BigDecimal("9.99")));
+        items.add(new Item("4", 10, new BigDecimal("9.99")));
+        items.add(new Item("4", 10, new BigDecimal("9.99")));
+        items.add(new Item("8", 10, new BigDecimal("9.99")));
 
 
-            new Item("orang", 10, new BigDecimal("29.99")),
-            new Item("watermelon", 10, new BigDecimal("29.99")),
-            new Item("papaya", 20, new BigDecimal("9.99"))
-    );
+
+
 
 //    Map<String, Long> counting = items.stream().collect(
 //            Collectors.groupingBy(Item::getName, Collectors.counting()));
@@ -45,20 +49,19 @@ public class Java8Examples3 {
 //        Map<String, List<Item>> groupByPriceMap =
 //                items.stream().collect(Collectors.groupingBy(Item::getName));
 
-
-        Map<String, List<Item>> groupByPriceMap2 =
-                items.stream().collect(Collectors.groupingBy(x->x.getName()+x.getId()));
-
-
-//        Set<Map.Entry<String, List<Item>>> entries = ggroupByPriceMap2.entrySet();
-        groupByPriceMap2.entrySet().stream().forEach(x->System.out.println(x.getKey()+"--------"+x.getValue()));
-
-
+//
+//        Map<String, List<Item>> groupByPriceMap2 =
+//                items.stream().collect(Collectors.groupingBy(x->x.getName()+x.getId()));
+//
+//
+////        Set<Map.Entry<String, List<Item>>> entries = ggroupByPriceMap2.entrySet();
+//        groupByPriceMap2.entrySet().stream().forEach(x->System.out.println(x.getKey()+"--------"+x.getValue()));
 
 
+        Map<String, List<Item>> collect = items.stream().collect(Collectors.groupingBy(x -> x.getName()));
 
 
-   
+
     }
 
 

74296a97b5b8c16119c44ab3f118b90eff65a0df
rqxiao <1007689622@qq.com>
2019-12-19 16:29:41 +0800

    New changelist

---
+++ src/main/java/ifelsetest.java
@@ -0,0 +1,29 @@
+/**
+ * @Author: rqxiao
+ * @Date: 2019-12-19 11:13
+ * @Description:
+ */
+public class ifelsetest {
+
+
+    public static void main(String[] args) {
+        int a=1;
+
+        if(a==1){
+            System.out.println("1");
+        }else if(a==2){
+            System.out.println("2");
+
+        }else if(a==3){
+            System.out.println("3");
+
+        }else if(a==4){
+            System.out.println("4");
+
+        }
+
+
+
+
+    }
+}

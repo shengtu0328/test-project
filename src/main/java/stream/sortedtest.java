0c8a9ef02b7ddb8a464a09bad9de72dacb96ef22
rqxiao <1007689622@qq.com>
2020-03-16 15:31:28 +0800

    New changelist

---
+++ src/main/java/stream/sortedtest.java
@@ -0,0 +1,33 @@
+package stream;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: rqxiao
+ * @Date: 2020-3-16 15:12
+ * @Description:
+ */
+public class sortedtest {
+
+    public static void main(String[] args) {
+
+        List<Developer> result = new ArrayList<Developer>();
+        result.add(new Developer("otto", new BigDecimal("1000"), 10));
+        result.add(new Developer("zed", new BigDecimal("1200"), 65));
+        result.add(new Developer("zed", new BigDecimal("1200"), 60));
+        result.add(new Developer("zed", new BigDecimal("1200"), 55));
+        result.add(new Developer("zed", new BigDecimal("1200"), 60));
+
+        result.add(new Developer("xrq", new BigDecimal("200"), 33));
+        result.add(new Developer("xrq2", new BigDecimal("700"), 33));
+        result.add(new Developer("bob", new BigDecimal("800"), 20));
+
+        List<Developer> collect = result.stream().sorted(Comparator.comparing(Developer::getSalary).thenComparing(Developer::getAge,Comparator.reverseOrder())).collect(Collectors.toList());
+        collect.stream().forEach(System.out::println);
+
+    }
+}

df4f749c588f870575208bfbb509e66bc0f35bfc
xiaoruiqing <xiaoruiqing@ehousechina.com>
2020-12-10 11:18:46 +0800

    mapstruct

---
+++ src/main/java/MapStruct/Person.java
@@ -0,0 +1,25 @@
+package MapStruct;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Person {
+    private Long id;
+    private String name;
+    private String email;
+    private Date birthday;
+    private User user;
+
+    public static void main(String[] args) {
+        Person person = new Person(1L,"zhige","zhige.me@gmail.com",new Date(),new User(1));
+        PersonDTO personDTO = PersonConverter.INSTANCE.domain2dto(person);
+        System.out.println(personDTO  );
+    }
+
+}
